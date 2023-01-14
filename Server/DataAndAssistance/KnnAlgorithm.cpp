937020428c5c1cebc9e7f0566335d8248096ef0e
TalMizrahii <Taltalon1927@gmail.com>
2023-01-14 20:18:07 +0200

    updated Knn.

--- Server/DataAndAssistance/KnnAlgorithm.cpp
+++ Server/DataAndAssistance/KnnAlgorithm.cpp
@@ -47,4 +47,84 @@ AbstractDistance *KnnAlgorithm::getCalc() {
     return this->calc;
 }
 
+/**
+ * A setter for the cataloged vector.
+ * @param catalogVectors The new catalog vector.
+ */
+void KnnAlgorithm::setCatalogVec(vector<ClassifiedRelativeVector *> catalogVectors) {
+    this->catalogedVectors = catalogVectors;
+}
+
+/**
+ *  A getter for the catalog vector.
+ * @return The catalog vector.
+ */
+vector<ClassifiedRelativeVector *> KnnAlgorithm::getCataloged() {
+    return this->catalogedVectors;
+}
+
+
+/**
+ * Given two files (as relativeVectors) calculating classifications for all unclassified files.
+ * @param UnclassifiedVectors The unclassified vectors.
+ * @param classifiedVectors The classified vectors.
+ * @return A vector of strings representing the classifications of all unclassified vectors.
+ * If the value is an empty vector, it means an error accord.
+ */
+vector<string> KnnAlgorithm::calculateFiles(vector<RelativeVector *> unclassifiedVectors,
+                                            vector<ClassifiedRelativeVector *> classifiedVectors) {
+    vector<string> resultClassifications;
+    // Setting the cataloged vector.
+    setCatalogVec(std::move(classifiedVectors));
+    string classification;
+    unsigned int size = unclassifiedVectors.size();
+    for(int i = 0; i < size; ++i){
+        classification = classifyVector(unclassifiedVectors[i]->getValuesVector());
+        if(classification.empty()){
+            vector<string> error;
+            return error;
+        }
+        resultClassifications.push_back(to_string(i) + " " + classification);
+    }
+    return resultClassifications;
+}
+
+/**
+ * The function check if two vectors are equally sized.
+ * @param v1 The first vector.
+ * @param v2 The second vector.
+ * @return A boolean true if they are equally sized, false otherwise.
+ */
+bool KnnAlgorithm::sizeComparison(const vector<double> &v1, const vector<double> &v2) {
+    // Checking if the vectors have the same size.
+    return (v1.size() == v2.size());
+}
+
+/**
+ * Calculate the distance between all vectors to the userVector and set them the result.
+ * @return true if the calculation worked, false otherwise.
+ */
+bool KnnAlgorithm::calculateDistances(vector<double> uVec) {
+    // Get the size of the cataloged vector.
+    unsigned int size = getCataloged().size();
+    // This for loop calc the distance between userVector(user input) to all the cataloged vectors.
+    for (int i = 0; i < size; i++) {
+        // Checking if the vectors in the same size.
+        if (!sizeComparison(getCataloged()[i]->getValuesVector(), uVec)) {
+            return false;
+        }
+        // Set the result of the distance between the user's vector to the ith vector.
+        double result = getCalc()->calculateDistance(getCataloged()[i]->getValuesVector(), uVec);
+        // Set the result to the ith vector.
+        getCataloged()[i]->setDistanceFromMasterVec(result);
+    }
+    return true;
+}
+
+
+string KnnAlgorithm::classifyVector(vector<double> unclassifiedVector) {
+
+}
+
+
 
--- Server/DataAndAssistance/KnnAlgorithm.h
+++ Server/DataAndAssistance/KnnAlgorithm.h
@@ -2,10 +2,13 @@
 #define EX4_KNNALGORITHM_H
 
 #include "Server/Distances/AbstractDistance.h"
+#include "ClassifiedRelativeVector.h"
 
 class KnnAlgorithm {
 public:
     KnnAlgorithm(AbstractDistance *calculation, unsigned long kNum);
+
+    ~KnnAlgorithm();
 
     void setKNeighbors(unsigned long kNum);
 
@@ -15,13 +18,28 @@ public:
 
     AbstractDistance *getCalc();
 
-    ~KnnAlgorithm();
+    void setCatalogVec(vector<ClassifiedRelativeVector *> catalogVectors);
+
+    vector<ClassifiedRelativeVector *> getCataloged();
+
+    string classifyVector(vector<double> unclassifiedVector);
+
+    bool KnnAlgorithm::calculateDistances(vector<double> uVec);
+
+    // Check validation of the vectors.
+    bool sizeComparison(const vector<double> &v1, const vector<double> &v2);
+
+    vector<string> calculateFiles(vector<RelativeVector *> UnclassifiedVectors,
+                                  vector<ClassifiedRelativeVector *> classifiedVectors);
+
 
 private:
     // The calculation for the distance.
     AbstractDistance *calc;
     // The K nearest neighbors.
     unsigned long kNeighbors;
+    // The classified relative vectors.
+    vector<ClassifiedRelativeVector *> catalogedVectors;
 };
 
 #endif //EX4_KNNALGORITHM_H
