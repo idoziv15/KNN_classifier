f209ff6eb73746eebc28761a57ba0b624f4be70e
TalMizrahii <Taltalon1927@gmail.com>
2023-01-14 19:43:36 +0200

    Starting to build the KnnAlgorithm.

--- Server/DataAndAssistance/KnnAlgorithm.h
+++ Server/DataAndAssistance/KnnAlgorithm.h
@@ -1,85 +1,27 @@
-#ifndef EX3_KNNALGORITHM_H
-#define EX3_KNNALGORITHM_H
+#ifndef EX4_KNNALGORITHM_H
+#define EX4_KNNALGORITHM_H
 
-#include <cstdlib>
-#include <vector>
-#include <string>
-#include "RelativeVector.h"
-#include "../Distances/AbstractDistance.h"
-#include <bits/stdc++.h>
-#include <utility>
-#include <map>
-#include <cmath>
+#include "Server/Distances/AbstractDistance.h"
 
-using namespace std;
-
-/**
- * Given a vector, finding the k nearest neighbors to it.
- */
 class KnnAlgorithm {
 public:
-    // The constructor of the class.
-    explicit KnnAlgorithm(vector<RelativeVector *> cataloged_vectors,
-                          unsigned long k_neighbors, AbstractDistance *calculation);
+    KnnAlgorithm(AbstractDistance *calculation, unsigned long kNum);
 
-    // Setter for catalogedVectors.
-    void setCatalogedVectors(vector<RelativeVector *> cataloged_vectors);
+    void setKNeighbors(unsigned long kNum);
 
-    // Setter for userVector.
-    void setUserVector(vector<double> user_vector);
+    void setCalc(AbstractDistance *calculation);
 
-    // Setter for Calc.
-    void seCalc(AbstractDistance *calculation1);
+    unsigned long getKNeighbors();
 
-    // Setter for kNeighbors.
-    void setKNeighbors(unsigned long k_neighbors);
-
-    // Calculate the distances between the userVector to all cataloged vectors(classified vectors).
-    bool calculateDistances();
-
-    // Getter for kNeighbors.
-    unsigned long getKNeighbors() const;
-
-    // Getter for userVector.
-    vector<double> getUserVector();
-
-    // Getter for catalogedVectors.
-    vector<RelativeVector *> getCatalogedVectors();
-
-    // Getter for calc.
     AbstractDistance *getCalc();
 
-    // Check validation of the vectors.
-    bool sizeComparison(const vector<double> &v1, const vector<double> &v2);
-
-    // Extracting the largest classification from the map.
-    string extractClassification(const map<string, int> &kMap);
-
-    // Sorting the vector by distances.
-    vector<RelativeVector *> sortingAndGettingK();
-
-    // Creating a map from the data vector.
-    map<string, int> createMap(vector<RelativeVector *> knn);
-
-    // The Destructor of the class.
     ~KnnAlgorithm();
-
-    // A control flow method for the class.
-    string classificationUserVec();
-
-    // Delete all data of the catalog vector.
-    void destroyKnn();
-
 
 private:
     // The calculation for the distance.
     AbstractDistance *calc;
-    // The user input vector.
-    vector<double> userVector;
-    // The classified vectors.
-    vector<RelativeVector *> catalogedVectors;
-    // The k neighbors we got as argument to the program.
+    // The K nearest neighbors.
     unsigned long kNeighbors;
 };
 
-#endif //EX3_KNNALGORITHM_H
+#endif //EX4_KNNALGORITHM_H
