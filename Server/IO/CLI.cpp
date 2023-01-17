ff1bdde21a5191e3d2fbdf221f31f7b776bb66e0
Yuval Arbel <Arbelyuval56@gmail.com>
2023-01-17 23:09:27 +0200

    notes and description for CLI class.

--- Server/IO/CLI.cpp
+++ Server/IO/CLI.cpp
@@ -1,46 +1,82 @@
 #include "CLI.h"
 
 #include <utility>
-
+/**
+ * Destructor for CLI class.
+ */
 CLI::~CLI() = default;
 
+/**
+ * Default constructor for CLI class.
+ */
 CLI::CLI() = default;
 
+/**
+ * A constructor for CLI class.
+ * @param IO which IO we are going to use (standard IO or socket IO).
+ */
 CLI::CLI(AbstractDefaultIO *IO) {
     setDefaultIO(IO);
 }
 
+/**
+ * Setter for IO member.
+ * @param IO which IO we are going to use (standard IO or socket IO).
+ */
 void CLI::setDefaultIO(AbstractDefaultIO *IO) {
     this->defaultIo = IO;
 }
 
+/**
+ * A getter for the IO member of CLI class.
+ * @return the IO member.
+ */
 AbstractDefaultIO *CLI::getDefaultIO() {
     return this->defaultIo;
 }
 
+/**
+ * A setter for the map that contain commands.
+ * @param newCommand - Map that contains all the commands.
+ */
 void CLI::setCommands(map<string, Commander *> newCommand) {
     this->commands = std::move(newCommand);
 }
 
+/**
+ * A getter for the map of commands.
+ * @return Map that contains all the commands.
+ */
 map<string, Commander *> CLI::getCommands() {
     return this->commands;
 }
 
-
+/**
+ * The control flow method - Printing the menu for the user, check his choice and execute this command.
+ */
 void CLI::start() {
+    // Ger the IO.
     AbstractDefaultIO *dIO = getDefaultIO();
+    // Creating the map with all the possibles commands.
     map<string, Commander *> commanders = initializeCommands(dIO);
+    // SEt the member with the new map that contains all the possibles commands.
     setCommands(commanders);
+    // Creating the menu.
     string menu = menuCreator();
+    // While tru to run the connection between the client and the server until the client ask to close the connection.
     while (true) {
+        // Send the menu to the client.
         dIO->write(menu);
+        // Checking the user choice.
         string response = dIO->read();
+        // Check if the user entered a valid choice and execute his request, otherwise sending the user error.
         Commander* command = processRequest(response);
         if (command == nullptr){
             dIO->write("invalid input\n");
             continue;
         }
         command->execute();
+        // Checking if the user want to close the connection to the server.
         if (response == "8" || response == "8\n"){
             // Free recourses and close client socket!!!!!!!!!.
             break;
@@ -49,8 +85,14 @@ void CLI::start() {
 
 }
 
+/**
+ * Creating the menu for the user.
+ * @return A string - menu string.
+ */
 string CLI::menuCreator() {
+    // The start line of the menu.
     string menu = "Welcome to the KNN Classifier Server. Please choose an option:\n";
+    // Getting the description of each command to add to the menu.
     menu += "1. " + this->commands["1"]->getDescription();
     menu += "2. " + this->commands["2"]->getDescription();
     menu += "3. " + this->commands["3"]->getDescription();
@@ -60,9 +102,15 @@ string CLI::menuCreator() {
     return menu;
 }
 
-
+/**
+ * Creating the command ready to execute.
+ * @param IO standard Io or socket Io.
+ * @return A map that contains all the possibles commands.
+ */
 map<string, Commander *> CLI::initializeCommands(AbstractDefaultIO *IO) {
+    // Init a map for all the commands.
     map<string, Commander *> initCommands;
+    // Init all the commands by calling their constructor.
     Commander *uploadFile = new UploadFile("upload an unclassified csv data file\n", IO);
     Commander *algorithmSetting = new AlgorithmSetting("algorithm setting\n", IO);
     Commander *classifyData = new ClassifyData("classify data\n", IO);
@@ -70,6 +118,7 @@ map<string, Commander *> CLI::initializeCommands(AbstractDefaultIO *IO) {
     Commander *downloadResult = new SendToDownload("download results\n", IO);
     Commander *exit = new EndConnection("exit\n", IO);
 
+    // Profiling a value for specific key ( by the order of the menu commands).
     initCommands["1"] = uploadFile;
     initCommands["2"] = algorithmSetting;
     initCommands["3"] = classifyData;
@@ -80,6 +129,11 @@ map<string, Commander *> CLI::initializeCommands(AbstractDefaultIO *IO) {
     return initCommands;
 }
 
+/**
+ * Checking the user choice of the menu, which command the user want to execute.
+ * @param clientChoice string that the user entered (key).
+ * @return The specify command the user want to execute.
+ */
 Commander *CLI::processRequest(string clientChoice) {
     clientChoice.pop_back();
     if (this->commands.find(clientChoice) == this->commands.end()){
--- Server/IO/CLI.h
+++ Server/IO/CLI.h
@@ -15,33 +15,49 @@
 #include "../Commands/UploadFile.h"
 
 using namespace std;
-
+/**
+ * CLI class - The control flow of the server, running all the command by user's choice.
+ */
 class CLI {
 public:
+    // The control flow method.
     void start();
 
+    // The default constructor for CLI class.
     CLI();
 
+    // Constructor CLI class.
     explicit CLI(AbstractDefaultIO *defaultIo);
 
+    // Destructor for CLI class.
     ~CLI();
 
+    // Setter for IO.
     void setDefaultIO(AbstractDefaultIO* IO);
 
+    // Getter for IO.
     AbstractDefaultIO* getDefaultIO();
 
+    // Creating the menu for the client.
     string menuCreator();
 
+    // Creating a map that contain all the commands.
     map<string, Commander *> initializeCommands(AbstractDefaultIO *IO);
 
+    // Setter for the specific command.
     void setCommands(map<string, Commander *> newCommand);
 
+    // Getter for the command.
     map<string, Commander *> getCommands();
 
+    // This method checking the user choice and process is request.
     Commander* processRequest(string clientChoice);
 
 private:
+    // IO member.
     AbstractDefaultIO* defaultIo;
+
+    // Map for all the commands.
     map<string, Commander *> commands;
 };
 
