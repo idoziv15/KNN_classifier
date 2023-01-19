57678fbfabd27c82c580d666bffe4ed83dda58a5
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2023-01-19 10:10:29 +0200

    created the default op abstract class.

--- Client/AbstractOperations.cpp
+++ Client/AbstractOperations.cpp
@@ -1,1 +1,13 @@
 #include "AbstractOperations.h"
+
+AbstractOperations::AbstractOperations(AbstractDefaultIO *defaultIo) {
+    setDio(defaultIo);
+}
+
+void AbstractOperations::setDio(AbstractDefaultIO *defaultIo) {
+    this->dio = defaultIo;
+}
+
+AbstractDefaultIO *AbstractOperations::getDio() {
+    return this->dio;
+}
--- Client/AbstractOperations.h
+++ Client/AbstractOperations.h
@@ -1,7 +1,19 @@
 #ifndef EX4_ABSTRACTOPERATIONS_H
 #define EX4_ABSTRACTOPERATIONS_H
+
+#include <IO/AbstractDefaultIO.h>
+
 class AbstractOperations{
 public:
+     explicit AbstractOperations(AbstractDefaultIO *defaultIo);
+     ~AbstractOperations();
+
     virtual void executeOp() = 0;
+
+    void setDio(AbstractDefaultIO *defaultIo);
+    AbstractDefaultIO* getDio();
+
+private:
+    AbstractDefaultIO *dio;
 };
 #endif //EX4_ABSTRACTOPERATIONS_H
